131b054aeb048477f29ef1105d6e8eb76893728e
VeronicaFrota <veronicafrota2011@hotmail.com>
2019-08-14 17:59:19 -0300

    Navegacao

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { IonicPage } from 'ionic-angular/navigation/ionic-page';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +11,11 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  login() {
+    //this.navCtrl.push('CategoriasPage')               // Navegação sendo utilizada com o push empilha uma pagina em cima da outra
+    this.navCtrl.setRoot('CategoriasPage')              // Navegação sendo utilizada com o setRoot para não empilhar uma pagina em cima da outra
   }
 
 }
