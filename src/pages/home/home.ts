a1bea7195e1380801bc0cb0bdaa7b904bbe34ebb
VeronicaFrota <veronicafrota2011@hotmail.com>
2019-08-14 19:35:53 -0300

    Desabilitar menun na tela inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,21 +1,30 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, MenuController } from 'ionic-angular';
 import { IonicPage } from 'ionic-angular/navigation/ionic-page';
 
 @IonicPage()
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	constructor(
+		public navCtrl: NavController,
+		public menu: MenuController
+	) {}
 
-  }
+	ionViewWillEnter() {									// Desabilitar o menu
+		this.menu.swipeEnable(false)
+	}
 
-  login() {
-    //this.navCtrl.push('CategoriasPage')               // Navegação sendo utilizada com o push empilha uma pagina em cima da outra
-    this.navCtrl.setRoot('CategoriasPage')              // Navegação sendo utilizada com o setRoot para não empilhar uma pagina em cima da outra
-  }
+	ionViewDidLeave() {										// Habilitar o menu
+		this.menu.swipeEnable(true)
+	}
+
+	login() {
+		//this.navCtrl.push('CategoriasPage')               // Navegação sendo utilizada com o push empilha uma pagina em cima da outra
+		this.navCtrl.setRoot('CategoriasPage')              // Navegação sendo utilizada com o setRoot para não empilhar uma pagina em cima da outra
+	}
 
 }
