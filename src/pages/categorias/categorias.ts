ffc5706c84e16c44170b03c5744eeec397ccd610
VeronicaFrota <veronicafrota2011@hotmail.com>
2019-08-16 21:15:14 -0300

    Listando categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,18 +1,31 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CategoriaService } from '../../services/domain/categoria.service';
 
 @IonicPage()
 @Component({
-  selector: 'page-categorias',
-  templateUrl: 'categorias.html',
+	selector: 'page-categorias',
+	templateUrl: 'categorias.html',
 })
 export class CategoriasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	constructor(
+		public navCtrl: NavController, 
+		public navParams: NavParams,
+		public categoriaService: CategoriaService
+		) {}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoriasPage');
-  }
+	// Quando a página terminar de ser carregada
+	ionViewDidLoad() {
+		this.categoriaService.findAll()
+			.subscribe( 
+				(response) => {
+					console.log(response)
+				},
+				(error) => {
+					console.log(error)
+				}
+			)
+	}
 
 }
